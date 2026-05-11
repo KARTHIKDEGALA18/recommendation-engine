d1785f8f25cad37299a0cd13194e91512f6da9f8
unknown <degalakarthik14@gmail.com>
2026-05-11 21:26:22 +0530

    Integrated dynamic JSON-based recommendation system

--- api/main.py
+++ api/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
+
+# Load sample recommendation data
+with open("./data/sample_users.json", "r") as file:
+    recommendations_data = json.load(file)
 
 @app.get("/")
 def home():
@@ -8,7 +13,15 @@ def home():
 
 @app.get("/recommend/{user_id}")
 def recommend(user_id: int):
+
+    user_id = str(user_id)
+
+    if user_id in recommendations_data:
+        return {
+            "user_id": user_id,
+            "recommended_items": recommendations_data[user_id]
+        }
+
     return {
-        "user_id": user_id,
-        "recommended_items": [101, 205, 309]
+        "message": "User not found"
     }
