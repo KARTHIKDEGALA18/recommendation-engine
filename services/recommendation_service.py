a71825686c763f59c93bf26c1f970f5b1867d268
unknown <degalakarthik14@gmail.com>
2026-05-11 21:54:14 +0530

    Enhanced recommendation system with product metadata

--- services/recommendation_service.py
+++ services/recommendation_service.py
@@ -3,11 +3,30 @@ import json
 with open("./data/sample_users.json", "r") as file:
     recommendations_data = json.load(file)
 
+with open("./data/products.json", "r") as file:
+    products_data = json.load(file)
+
 def get_recommendations(user_id):
 
     user_id = str(user_id)
 
-    if user_id in recommendations_data:
-        return recommendations_data[user_id]
+    if user_id not in recommendations_data:
+        return []
 
-    return []
+    recommended_products = []
+
+    for product_id in recommendations_data[user_id]:
+
+        product_id = str(product_id)
+
+        if product_id in products_data:
+
+            product = {
+                "id": product_id,
+                "name": products_data[product_id]["name"],
+                "category": products_data[product_id]["category"]
+            }
+
+            recommended_products.append(product)
+
+    return recommended_products
